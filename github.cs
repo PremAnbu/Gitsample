ee34d9eb6be857e43b1a3eb0120257d18fa8c780
PremAnbu <premanbu9360@gmail.com>
2024-02-05 16:06:43 +0530

    created github class

---
+++ github.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Github
+{
+    internal class github
+    {
+        public static void Main()
+        {
+            int a = 10;
+            Console.WriteLine(a);
+        }
+    }
+}
